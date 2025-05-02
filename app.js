12cfbd03a26c3266d9c582d9cc402f9421188d40
SanjayIBAP <sanjaythehero2001@gmail.com>
2025-05-02 11:31:51 +0530

    Add root test route

--- app.js
+++ app.js
@@ -12,4 +12,8 @@ app.use(express.json());
 app.use('/api/contact', contactRoutes);
 
 const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('API is running ✅');
+  });
+  
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
